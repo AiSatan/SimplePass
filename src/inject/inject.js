e97ae584aaf0c313672d863d2d793efd4b65d0dc
AiSatan <aisatan.ne@gmail.com>
2015-03-03 17:22:20 +0300

    added twittor suppot

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -7,9 +7,17 @@ chrome.extension.sendMessage({}, function (response) {
 		// This part of the script triggers when page is done loading
 		console.log("Hello. This message was sent from scripts/inject.js");
 	    // ----------------------------------------------------------
-
+		
 	    FindPasswordBox();
 	}
+	}, 10);
+
+	setInterval(function () {
+	    if (document.readyState === "complete") {
+	        if (document.location.href === "https://twitter.com/following") {
+	            SearchReader();
+	        }
+	    }
 	}, 10);
 });
 
@@ -21,4 +29,17 @@ function FindPasswordBox() {
         border: "2px red solid"
     });
     //passwordBox.val("TEST");
+}
+
+function SearchReader() {
+    var readers = $(".ProfileCard-screenname");
+    readers.each(function () {
+        if ($(this).children().next().attr('class') !== "FollowStatus") {
+            $(this).css({
+                background: "yellow",
+                border: "2px red solid"
+            });
+        }
+    });
+    
 }
